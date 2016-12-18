62d11319a6528067ec5158edcca71e3b354f5193
Mauricio Cuenca <macuenca@gmail.com>
2016-12-18 16:05:26 +0000

    Specify viewport for better display on mobile devices.

--- resources/views/layouts/chat.blade.php
+++ resources/views/layouts/chat.blade.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Laravel') }}</title>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
